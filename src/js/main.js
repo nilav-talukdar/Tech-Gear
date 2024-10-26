2231fabd53744c0bb994578cc3d0f80de1686299
nilav-talukdar <nilavtalukdar49@gmail.com>
2024-10-26 22:02:42 +0530

    Added a typing effect

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,16 @@
 import '../scss/styles.scss';
 import '../../css/style.css';
 import * as bootstrap from 'bootstrap';
+import Typewriter from 'typewriter-effect/dist/core';
 
 const brandName = document.querySelector('.navbar-brand');
 brandName.addEventListener('click', () => {
     window.location.reload();
 });
+
+new Typewriter('#type', {
+    strings: ['the Future of Tech'],
+    loop: true,
+    autoStart: true,
+    delay: 50,
+  });
