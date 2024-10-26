ece2a8853892ec1471e0ce5fb64ecdf0a373d918
nilav-talukdar <nilavtalukdar49@gmail.com>
2024-10-26 23:24:55 +0530

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,8 @@
 import '../scss/styles.scss';
 import '../../css/style.css';
 import * as bootstrap from 'bootstrap';
-import Typewriter from 'typewriter-effect/dist/core';
 
 const brandName = document.querySelector('.navbar-brand');
 brandName.addEventListener('click', () => {
     window.location.reload();
 });
-
-/* new Typewriter('#type', {
-    strings: ['the Future of Tech'],
-    loop: true,
-    autoStart: true,
-    delay: 50,
-  }); */
